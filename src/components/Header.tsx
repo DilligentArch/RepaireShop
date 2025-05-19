9a0ff1d1401722338e509ec56ab8ab1d6cf62632
Nayeb Ahmed Qureshi <nayebqureshi4@gmail.com>
2025-05-20 01:16:52 +0600

    2nd commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,27 @@
+import { File, HomeIcon, UserRound } from "lucide-react";
+import { NavButton } from "./NavButton";
+import Link from "next/link";
+
 export function Header() {
     return (
-        <p>Header</p>
+        <header className="animate-slide bg-background h-12 p-2border-b sticky top-0 z-20">
+            <div className="flex h-8 itmes-center justify-between w-full">
+
+                <div className="flex items-center gap-2">
+                    <NavButton href="/home" label="Home" icon={HomeIcon}/>
+                        <Link href="/home" className="flex justify-center items-center gap-2 ml-0" title="Home">
+                            <h1 className="hidden sm:block text-xl font-bold m-0 mt-1 ">
+                                Compuyter Repair shop
+                            </h1>
+                        </Link>
+                    
+                </div>
+                <div className="flex items-center ">
+                    <NavButton href="/tickets" label="Tickets" icon={File}/>
+                    <NavButton href="/customers" label="Customers" icon={UserRound}/>
+                </div>
+            </div>
+        </header>
+
     );
 }
