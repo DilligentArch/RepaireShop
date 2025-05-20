e53da614352bcc8d5e67cf5d3fe7b326836ffadf
Nayeb Ahmed Qureshi <nayebqureshi4@gmail.com>
2025-05-21 02:52:35 +0600

    finished auth

--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -1,3 +1,11 @@
+export const metadata = {
+    title: "Home",
+    description: "Home page",
+    };
+
+
+
+
 export default function Home() {
  return <h2>Home page</h2>
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
-import { File, HomeIcon, UserRound } from "lucide-react";
+import { File, HomeIcon, LogOut, UserRound } from "lucide-react";
 import { NavButton } from "./NavButton";
 import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+import { Button } from "./ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export function Header() {
+   
     return (
         <header className="animate-slide bg-background h-12 p-2border-b sticky top-0 z-20">
             <div className="flex h-8 itmes-center justify-between w-full">
@@ -19,9 +23,24 @@ export function Header() {
                 <div className="flex items-center ">
                     <NavButton href="/tickets" label="Tickets" icon={File}/>
                     <NavButton href="/customers" label="Customers" icon={UserRound}/>
+                    <ModeToggle/>
+
+
+                    <Button 
+                        variant="ghost"
+                        size="icon"
+                        aria-label="Logout"
+                        title="Logout"
+                        className="rounded-full"
+                        asChild
+                    >
+                        <LogoutLink> <LogOut></LogOut></LogoutLink>
+                    </Button>
                 </div>
             </div>
         </header>
 
     );
 }
+
+
